deb1a91029773b656f8779e2e17a553af626d2fd
huseyincakir54 <huseyin.caqir@gmail.com>
2026-02-04 02:56:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from 'path';
+lentiimport path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/REPO_ADI/",
+  base: "/eklenti/",
 });
