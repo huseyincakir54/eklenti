492d0a651e85b2569cf624caab6246c56b8e8dd5
huseyincakir54 <huseyin.caqir@gmail.com>
2026-02-04 03:11:56 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-lentiimport path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/eklenti/",
-});
